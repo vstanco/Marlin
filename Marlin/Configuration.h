fb72cbfc6e8e037593d6ed010ce2576f917c7041
vstanco <67430667+vstanco@users.noreply.github.com>
2021-02-25 19:14:23 +0100

    300x200

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -629,10 +629,10 @@
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
-//#define USE_YMIN_PLUG
+#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
-#define USE_YMAX_PLUG
+//#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
 // Enable pullup for all endstops to prevent a floating state
@@ -752,8 +752,8 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 
-//* Hemera
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 635, 807, 1620, 409 }
+//* Hemera(0,9/1,8/0,9)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 635, 403, 1620, 409 }
 
 //* EZR 1.8 stepper
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 635, 807, 1620, 95 }
@@ -1144,7 +1144,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 
 // @section extruder
@@ -1173,18 +1173,18 @@
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
 #define X_HOME_DIR -1
-#define Y_HOME_DIR 1
+#define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
 // @section machine
 
 // The size of the print bed
 #define X_BED_SIZE 300
-#define Y_BED_SIZE 310
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
